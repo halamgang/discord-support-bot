f1cafc831b66481d0ea66234b97f252bf395c380
halamgang <73386150+halamgang@users.noreply.github.com>
2023-03-12 11:33:07 +0900

    디스코드 봇
    environ 수정

--- discordbot.py
+++ discordbot.py
@@ -1,32 +1,62 @@
-from cmath import log
-from distutils.sysconfig import PREFIX
-import discord
-from dotenv import load_dotenv
 import os
-load_dotenv()
+import discord
 
-PREFIX = os.environ['PREFIX']
-TOKEN = os.environ['TOKEN']
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
-client = discord.Client()
+SUPPORT_CATEGORY_NAME = "support"  # 변경 가능
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}.')
+    print(f'{client.user} has connected to Discord!')
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    if message.content.startswith('!지원'):
+        support_role_name = "Support"  # 변경 가능
+        support_category = discord.utils.get(message.guild.categories, name=SUPPORT_CATEGORY_NAME)
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
+        if support_category is None:
+            await message.channel.send(f"카테고리 '{SUPPORT_CATEGORY_NAME}'을(를) 찾을 수 없습니다.")
+            return
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+        support_role = discord.utils.get(message.guild.roles, name=support_role_name)
 
+        if support_role is None:
+            await message.channel.send(f"역할 '{support_role_name}'을(를) 찾을 수 없습니다.")
+            return
 
-try:
-    client.run(TOKEN)
-except discord.errors.LoginFailure as e:
-    print("Improper token has been passed.")
+        overwrites = {
+            message.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            message.guild.me: discord.PermissionOverwrite(read_messages=True),
+            support_role: discord.PermissionOverwrite(read_messages=True),
+            message.author: discord.PermissionOverwrite(read_messages=True)
+        }
+
+        support_channel_name = f"{message.author.display_name}-지원"
+
+        try:
+            support_channel = await message.guild.create_text_channel(support_channel_name, category=support_category, overwrites=overwrites)
+            await support_channel.send(f"{message.author.mention}님, 지원 채널이 생성되었습니다. 문의사항을 적어주세요.")
+            await message.delete()
+        except discord.errors.Forbidden:
+            await message.channel.send("Bot에 권한이 없어 채널을 만들 수 없습니다.")
+    
+    elif message.channel.category.name == SUPPORT_CATEGORY_NAME and message.author != client.user:
+        support_role_name = "Support"  # 변경 가능
+        support_role = discord.utils.get(message.guild.roles, name=support_role_name)
+
+        if support_role is None:
+            await message.channel.send(f"역할 '{support_role_name}'을(를) 찾을 수 없습니다.")
+            return
+
+        if message.author == support_role or support_role in message.author.roles:
+            if message.content.startswith("!삭제"):
+                await message.channel.delete()
+            else:
+                await message.add_reaction('✅')
+        else:
+            await message.add_reaction('❌')
+
+def run():
+    client.run(os.environ['TOKEN'])
